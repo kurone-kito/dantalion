0028c20f7f39d1aa628f36d8443990fecc4d7e16
kurone-kito <krone@kit.black>
2020-03-13 01:42:55 +0900

    feat: implemented the entry point

--- packages/dantalion-core/src/index.ts
+++ packages/dantalion-core/src/index.ts
@@ -1,1 +1,7 @@
-export default () => 'Hello, world!';
+import details from './records/details';
+import { Genius } from './types/genius';
+
+export { Genius } from './types/genius';
+export { LifeBase } from './types/lifeBase';
+export { Potential } from './types/potential';
+export { default as getPersonality, Personality } from './utils/getPersonality';
