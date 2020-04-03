42684ef83ef2bd76e420e45db70c451aa899ac26
kurone-kito <krone@kit.black>
2020-04-04 01:11:57 +0900

    feat: added the functions for create genius record

---
+++ packages/dantalion-core/src/utils/createGeniusRecords.ts
@@ -0,0 +1,9 @@
+import createGeniusRecord from './createGeniusRecord';
+
+/**
+ * Create a record for each genius type from the source.
+ * @param table Source.
+ * @template T Type of the source.
+ */
+export default <T>(table: T[][]) =>
+  createGeniusRecord(table.map(row => createGeniusRecord(row)));
