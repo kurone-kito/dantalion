746de9523c76375adf3821e37e68c8e635bea240
kurone-kito <krone@kit.black>
2020-04-08 01:12:45 +0900

    feat: improved the entry point

--- packages/dantalion-core/src/index.ts
+++ packages/dantalion-core/src/index.ts
@@ -1,7 +1,18 @@
 import details from './records/details';
 import { Genius } from './types/genius';
 
+export { Affinity, Detail } from './records/details';
+export { AffinityLevel } from './types/AffinityLevel';
+export { Brain } from './types/brain';
+export { Communication } from './types/communication';
 export { Genius } from './types/genius';
 export { LifeBase } from './types/lifeBase';
+export { Management } from './types/management';
+export { Motivation } from './types/motivation';
+export { Position } from './types/position';
 export { Potential } from './types/potential';
+export { Response } from './types/response';
+export { Vector } from './types/vector';
 export { default as getPersonality, Personality } from './utils/getPersonality';
+
+export const getDetail = (genius: Genius) => details[genius];
