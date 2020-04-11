43a349f16006722436bd58860e2069837b37a347
kurone-kito <krone@kit.black>
2020-04-12 01:10:34 +0900

    test: manually linted

--- packages/dantalion-core/src/index.spec.ts
+++ packages/dantalion-core/src/index.spec.ts
@@ -5,13 +5,13 @@ import { getPersonalityTestData } from './tests';
 describe('integration testing', () => {
   describe('get the personality', () => {
     it('Outputs the same value as the data source from all dates in the range', () => {
-      for (const source of getPersonalityTestData()) {
+      getPersonalityTestData().forEach(source => {
         const result = getPersonality(source.date)!;
         expect({ ...result, date: source.date }).toStrictEqual({
           ...source,
           lifeBase: source.lifeBase ?? result.lifeBase
         });
-      }
+      });
     });
   });
 });
