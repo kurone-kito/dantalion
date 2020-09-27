64481e7c774d95885cbbfbe6387bbdbc43aba5da
kurone-kito <krone@kit.black>
2020-09-27 13:57:30 +0900

    feat: added the description of the genius

--- packages/dantalion-i18n/src/index.ts
+++ packages/dantalion-i18n/src/index.ts
@@ -1,5 +1,19 @@
+import { Genius } from '@kurone-kito/dantalion-core';
 import getResources from './resources';
 
-getResources().then((t) => {
-  console.log(t('hello', { returnObjects: true }));
-});
+export interface GeniusType {
+  detail: string[];
+  keyword: string[];
+  name: string;
+  summary: string;
+  strategy: string[];
+  weak: string[];
+}
+
+export const getGeniusAsync = async (genius: Genius) => {
+  const result = (await getResources())<string | GeniusType>(
+    `genius.${genius}`,
+    { returnObjects: true }
+  );
+  return typeof result === 'string' ? undefined : result;
+};
