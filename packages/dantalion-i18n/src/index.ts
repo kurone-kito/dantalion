d0eb33a504ca8884399259ae1ce25e3942a6ed8b
kurone-kito <krone@kit.black>
2021-04-23 22:06:20 +0900

    refactor: unified the resources access function

--- packages/dantalion-i18n/src/index.ts
+++ packages/dantalion-i18n/src/index.ts
@@ -1,21 +1,7 @@
 import type { Genius } from '@kurone-kito/dantalion-core';
-import getResources from './resources/t';
+import type { PersonalityType } from './resources/types';
+import getResourcesAccessor from './resources';
 
-export interface GeniusType {
-  detail: string[];
-  keyword: string[];
-  name: string;
-  summary: string;
-  strategy: string[];
-  weak: string[];
-}
-
-export const getGeniusAsync = async (
-  genius: Genius
-): Promise<GeniusType | undefined> => {
-  const result = (await getResources())<string | GeniusType>(
-    `genius.${genius}`,
-    { returnObjects: true }
-  );
-  return typeof result === 'string' ? undefined : result;
-};
+export const getGeniusAsync = getResourcesAccessor<PersonalityType, Genius>(
+  'genius'
+);
---
+++ packages/dantalion-i18n/src/resources/index.ts
@@ -0,0 +1,58 @@
+import getResourcesAsync from './t';
+import type { DetailsBaseType } from './types';
+
+/**
+ * The type definition with a function to access
+ * a resource of the specific category.
+ * @template T The type of resource as a return value.
+ * @template K The type for the key.
+ */
+export interface ResourcesAccessor<
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  T extends object | string,
+  K extends string
+> {
+  /**
+   * The function acquires the resource corresponding to the key asynchronously.
+   */
+  readonly getAsync: (
+    /** The key. */
+    key: K
+  ) => Promise<T | undefined>;
+  /**
+   * The function acquires the resource corresponding
+   * to the specific category asynchronously.
+   */
+  readonly getCategoryDetailAsync: () => Promise<DetailsBaseType | undefined>;
+}
+
+/**
+ * The function acquires the resource corresponding to the key asynchronously.
+ * @template T The type of resource as a return value.
+ * @param key The key.
+ * @returns The resources object.
+ *
+ * If the resource associated with the specified key does not exist,
+ * the function returns an undefined value.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+const getAsync = async <T extends object>(key: string) => {
+  const t = await getResourcesAsync();
+  const result = t<string | T>(key, { returnObjects: true });
+  return typeof result === 'string' ? undefined : result;
+};
+
+/**
+ * Create the accessor functions that acquire the resource corresponding
+ * to the specified category asynchronously.
+ * @template T The type of resource as a return value.
+ * @template K The type for the key.
+ * @param category The category key.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export default <T extends object, K extends string>(
+  category: string
+): ResourcesAccessor<T, K> => ({
+  getCategoryDetailAsync: () => getAsync<DetailsBaseType>(`${category}.detail`),
+  getAsync: (key: K) => getAsync<T>(`${category}.${key}`),
+});
