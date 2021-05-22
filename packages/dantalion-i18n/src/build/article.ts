74d7f521aceeb8de7c6dbdf388b13232cd075cc9
kurone-kito <krone@kit.black>
2021-05-22 10:41:23 +0900

    refactor: removed features that are virtually unnecessary on build the Markdown

--- packages/dantalion-i18n/src/build/article.ts
+++ packages/dantalion-i18n/src/build/article.ts
@@ -2,15 +2,10 @@ import { line } from './list';
 
 /** The options for the `article` function. */
 export interface Options {
-  /**
-   * The body text.
-   *
-   * If this value is an array, it will be rendered multiple lines,
-   * one element at a line.
-   */
-  readonly body?: string | readonly (string | undefined)[];
+  /** The body text. */
+  readonly body?: string;
   /** The heading text. */
-  readonly head?: string;
+  readonly head: string;
   /**
    * The heading level.
    *
@@ -23,9 +18,5 @@ export interface Options {
  * Create the Markdown from the heading and body pair.
  * @param options The options.
  */
-export default ({ body = '', head = '', level = 1 }: Options = {}): string =>
-  line(
-    `${'#'.repeat(level)} ${head}`.trim(),
-    '',
-    (typeof body === 'string' ? body : line(...body)).trim()
-  );
+export default ({ body = '', head, level = 1 }: Options): string =>
+  line(`${'#'.repeat(level)} ${head}`.trim(), '', body.trim());
