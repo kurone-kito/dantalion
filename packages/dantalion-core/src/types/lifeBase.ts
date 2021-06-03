16a6b97bc58d2284caa4b40ebf41b784d152e341
kurone-kito <krone@kit.black>
2021-06-04 08:53:28 +0900

    docs: added the like CC code

--- packages/dantalion-core/src/types/lifeBase.ts
+++ packages/dantalion-core/src/types/lifeBase.ts
@@ -1,18 +1,18 @@
 /**
  * The types that the base of ego.
  *
- * |      Key       | Value                                                                    |
- * | :------------: | :----------------------------------------------------------------------- |
- * | `application`  | This type of person would like to self-experience seriously.             |
- * | `association`  | This type of person would like to do it immediately when they think.     |
- * | `development`  | This type of person would like to be perfectionists.                     |
- * |  `expression`  | This type of person would like to be honest with themselves.             |
- * |   `finance`    | This type of person would like to put everything within eye reach.       |
- * |  `investment`  | This type of person would like to be a down-to-earth collector.          |
- * | `organization` | This type of person would like to live as a member of a group.           |
- * |    `quest`     | This type of person would like to learn from the wisdom of our pioneers. |
- * |   `selfMind`   | This type of person would like to be a leader of the team.               |
- * | `selfReliance` | This type of person would like to be a lone wolf.                        |
+ * | Key            | CC  | Value                                                                    |
+ * | :------------- | :-- | :----------------------------------------------------------------------- |
+ * | `application`  | _G_ | This type of person would like to self-experience seriously.             |
+ * | `association`  | _I_ | This type of person would like to do it immediately when they think.     |
+ * | `development`  | _D_ | This type of person would like to be perfectionists.                     |
+ * | `expression`   | _C_ | This type of person would like to be honest with themselves.             |
+ * | `finance`      | _E_ | This type of person would like to put everything within eye reach.       |
+ * | `investment`   | _F_ | This type of person would like to be a down-to-earth collector.          |
+ * | `organization` | _H_ | This type of person would like to live as a member of a group.           |
+ * | `quest`        | _J_ | This type of person would like to learn from the wisdom of our pioneers. |
+ * | `selfMind`     | _B_ | This type of person would like to be a leader of the team.               |
+ * | `selfReliance` | _A_ | This type of person would like to be a lone wolf.                        |
  */
 export type LifeBase =
   | 'application'
