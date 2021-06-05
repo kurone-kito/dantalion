bac41f42345a7fac7bb46bd682033de6e0de2c17
kurone-kito <krone@kit.black>
2021-06-05 21:31:57 +0900

    feat: added the stories of the components of the molecules

---
+++ packages/dantalion-web-playground/src/components/molecules/Article.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/react';
+import Article, { Props } from './Article';
+
+export default Object.freeze<Meta>({
+  component: Article,
+  title: `molecules/${Article.displayName}`,
+});
+
+const Template: Story<Props> = ({
+  children,
+  featureBody,
+  featureHeading,
+  tooltipFeatureDetails,
+}) => (
+  <Article
+    featureBody={featureBody}
+    featureHeading={featureHeading}
+    tooltipFeatureDetails={tooltipFeatureDetails}
+  >
+    {children}
+  </Article>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  featureBody: ['Foo', 'Bar', 'Qux'],
+  children: '_Markdown_ **contents**',
+  featureHeading: 'FeatureHeading',
+  tooltipFeatureDetails: 'TooltipFeatureDetails',
+};
---
+++ packages/dantalion-web-playground/src/components/molecules/InlineMarkdownList.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react';
+import InlineMarkdown, { Props } from './InlineMarkdownList';
+
+export default Object.freeze<Meta>({
+  component: InlineMarkdown,
+  title: `molecules/${InlineMarkdown.displayName}`,
+});
+
+const Template: Story<Props> = ({ children, className, itemType, order }) => (
+  <InlineMarkdown className={className} itemType={itemType} order={order}>
+    {children}
+  </InlineMarkdown>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: ['**Foo**', '_Bar_', 'Qux'],
+  className: 'list-disc text-red-900',
+  order: false,
+};
