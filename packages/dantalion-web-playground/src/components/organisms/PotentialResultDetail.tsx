fab1565879f1ed2585fdbbb561f1a6f7ee2dde41
kurone-kito <krone@kit.black>
2021-06-14 09:02:07 +0900

    feat: implemented the potentials detail for web

---
+++ packages/dantalion-web-playground/src/components/organisms/PotentialResultDetail.tsx
@@ -0,0 +1,35 @@
+import type { Potential } from '@kurone-kito/dantalion-core';
+import type { DetailAccessor } from '@kurone-kito/dantalion-i18n';
+import type { VFC } from 'react';
+import { useTranslation } from 'react-i18next';
+import ResultDetail from '../molecules/ResultDetail';
+
+/** Type definition of the required attributes. */
+export interface Props {
+  /** Specifies the accessor object */
+  readonly accessors: DetailAccessor<
+    readonly string[],
+    readonly [Potential, Potential]
+  >;
+  /** Specifies the lifebase value */
+  readonly potentials: readonly [Potential, Potential];
+  /** Specifies the nickname. */
+  readonly nickname?: string;
+}
+
+/** The result component. */
+const Component: VFC<Props> = ({ accessors, potentials, nickname }) => {
+  const { t } = useTranslation();
+  const desc = accessors.getCategoryDetail();
+  return (
+    <ResultDetail
+      heading={desc.name}
+      headingDetail={desc.detail}
+      hook={t('web.result.potential', { nickname })}
+      moreDetail={accessors.getByKey(potentials)}
+    />
+  );
+};
+Component.displayName = 'LifeBaseResultDetail';
+
+export default Component;
