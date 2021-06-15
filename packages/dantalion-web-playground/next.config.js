c5a898d0d4be9902c020cf8b71a71c86529c38d8
kurone-kito <krone@kit.black>
2021-06-15 20:52:53 +0900

    fix: fixed the Sentry tracking was not working

--- packages/dantalion-web-playground/next.config.js
+++ packages/dantalion-web-playground/next.config.js
@@ -1,11 +1,16 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
 const productDomain = 'https://kurone-kito.github.io';
 const productPath = '/dantalion/';
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+/** @type {Partial<import('next/dist/next-server/server/config-shared').NextConfig>} */
+const providedExports = {
   assetPrefix: isProd ? productDomain + productPath : '',
   env: { productDomain, productPath },
   future: { webpack5: true },
   // i18n: { defaultLocale: 'en', locales: ['en', 'ja'] },
   reactStrictMode: true,
 };
+
+module.exports = withSentryConfig(providedExports, {});
