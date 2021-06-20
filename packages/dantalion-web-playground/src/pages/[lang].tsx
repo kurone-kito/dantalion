e524aa1769fb2397bbf9da8444afbf1d18d2c1c0
kurone-kito <krone@kit.black>
2021-06-21 08:55:56 +0900

    feat: added the page each by genius type

--- packages/dantalion-web-playground/src/pages/[lang].tsx
+++ packages/dantalion-web-playground/src/pages/[lang].tsx
@@ -1,3 +1,4 @@
+import { Genius, types } from '@kurone-kito/dantalion-core';
 import { locales } from '@kurone-kito/dantalion-i18n';
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
@@ -5,20 +6,39 @@ import Template from '../components/templates/Template';
 import type { PageProps } from './_app';
 
 /** Type definition of the required attributes. */
-export type Query = { readonly lang: string };
+export type Query = {
+  /** Specifies the language of this page. */
+  readonly lang?: string;
+};
 
 /** The index page component */
-const Component: NextPage<PageProps> = () =>
-  useRouter().isFallback ? <>Loading...</> : <Template />;
+const Component: NextPage<PageProps> = ({ genius }) =>
+  useRouter().isFallback ? <>Loading...</> : <Template inner={genius} />;
 Component.displayName = '[lang]';
 
+/**
+ * Determine the attributes to assign to each path.
+ * @param context The context.
+ */
 export const getStaticProps: GetStaticProps<PageProps, Query> = async ({
   params,
-}) => ({ props: { lang: params?.lang } });
+}) => {
+  const isLang = Number.isNaN(Number.parseInt(params?.lang ?? '', 10));
+  return {
+    props: {
+      ...(isLang ? {} : { genius: params?.lang as Genius | undefined }),
+      ...(isLang ? { lang: params?.lang } : {}),
+    },
+  };
+};
 
+/** Determine the path to pre-render based on the data. */
 export const getStaticPaths: GetStaticPaths<Query> = async () => ({
   fallback: false,
-  paths: Object.keys(locales).map((lang) => ({ params: { lang } })),
+  paths: [
+    Object.keys(locales).map((lang) => ({ params: { lang } })),
+    types.genius.map((lang) => ({ params: { lang } })),
+  ].flat(),
 });
 
 export default Component;
